3bed1959d15350d3868692d14d94783d55037d80
Le Tho <leductho1812@gmail.com>
2024-08-13 16:54:59 +0700

    crop

--- app/src/main/java/com/example/mviimageeditor/custom/CropView.kt
+++ app/src/main/java/com/example/mviimageeditor/custom/CropView.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -18,7 +22,9 @@ import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun CropView(
@@ -28,146 +34,185 @@ fun CropView(
     onDrag3rdQuad: () -> Unit,
     onDrag4thQuad: () -> Unit,
 ) {
+    var boxSize2 by remember {
+        mutableStateOf(DpSize(100.dp, 100.dp))
+    }
     Box(
         modifier = modifier
     ) {
-        Row(modifier = Modifier.size(100.dp).background(Color.Transparent).align(
-            Alignment.Center
-        )){
-            Canvas(
+        ConstraintLayout(modifier = Modifier.align(Alignment.Center)) {
+            val (box1, box2, box3, box4) = createRefs()
+            Box(
                 modifier = Modifier
-                    .size(80.dp)
-                    .padding(top = 10.dp, start = 10.dp)
-
-                    .pointerInput(key1 = onDrag2ndQuad) {
-                        detectDragGestures(
-                            onDrag = { change: PointerInputChange, dragAmount: Offset ->
-                                onDrag2ndQuad.invoke(dragAmount)
+                    .size(boxSize2)
+                    .background(Color.Transparent)
+                    .constrainAs(box2) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+            ) {
+                Canvas(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(top = 10.dp, start = 10.dp)
+                        .pointerInput(key1 = onDrag2ndQuad) {
+                            detectDragGestures(
+                                onDrag = { change: PointerInputChange, dragAmount: Offset ->
+                                    boxSize2 = boxSize2.minus(DpSize(dragAmount.x.dp, dragAmount.y.dp))
+                                //    onDrag2ndQuad.invoke(dragAmount)
 //                            onDragDirection(
 //                                dragAmount,
 //                                onDragRight = {},
 //                                onDragLeft = {},
 //                                onDragTop = {},
 //                                onDragBottom = {})
-                            }
+                                }
+                            )
+                        }
+                ) {
+                    val width = size.width
+                    val height = size.height
+                    val path = Path().apply {
+                        moveTo(0f, height)  // Điểm bắt đầu của đường đầu tiên
+                        lineTo(0f, height / 2)  // Đường ngang đầu tiên
+                        quadraticTo(0f, 0f, width / 2, 0f) // Đường cong Bézier để tạo bo góc
+                        lineTo(width, 0f)  // Đường thẳng đứng thứ hai
+                    }
+
+                    drawPath(
+                        path = path,
+                        color = Color.Black,
+                        style = Stroke(
+                            width = 10f,
+                            cap = StrokeCap.Round,
+                            join = StrokeJoin.Round
                         )
+                    )
+                }
+            }
+            Box(modifier = Modifier
+                .size(100.dp)
+                .background(Color.Transparent)
+                .constrainAs(box1) {
+                    top.linkTo(box2.top)
+                    start.linkTo(box2.end)
+                }) {
+                Canvas(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(top = 10.dp, end = 10.dp)
+
+                ) {
+                    val width = size.width
+                    val height = size.height
+                    val path = Path().apply {
+                        moveTo(width, height)  // Điểm bắt đầu của đường đầu tiên
+                        lineTo(width, height / 2)  // Đường ngang đầu tiên
+                        quadraticTo(
+                            width,
+                            0f,
+                            width / 2,
+                            0f
+                        ) // Đường cong Bézier để tạo bo góc
+                        lineTo(0f, 0f)  // Đường thẳng đứng thứ hai
+                    }
+
+                    drawPath(
+                        path = path,
+                        color = Color.Black,
+                        style = Stroke(
+                            width = 10f,
+                            cap = StrokeCap.Round,
+                            join = StrokeJoin.Round
+                        )
+                    )
+                }
+            }
+
+
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(Color.Transparent)
+                    .constrainAs(box4) {
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(box1.end)
+                        top.linkTo(box1.bottom)
                     }
             ) {
-                val width = size.width
-                val height = size.height
-                val path = Path().apply {
-                    moveTo(0f, height)  // Điểm bắt đầu của đường đầu tiên
-                    lineTo(0f, height / 2)  // Đường ngang đầu tiên
-                    quadraticTo(0f, 0f, width / 2, 0f) // Đường cong Bézier để tạo bo góc
-                    lineTo(width, 0f)  // Đường thẳng đứng thứ hai
-                }
+                Canvas(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(bottom = 10.dp, end = 10.dp)
 
-                drawPath(
-                    path = path,
-                    color = Color.Black,
-                    style = Stroke(
-                        width = 10f,
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Round
+                ) {
+                    val width = size.width
+                    val height = size.height
+                    val path = Path().apply {
+                        moveTo(0f, height)  // Điểm bắt đầu của đường đầu tiên
+                        lineTo(width / 2, height)  // Đường ngang đầu tiên
+                        quadraticTo(
+                            width,
+                            height,
+                            width,
+                            height / 2
+                        ) // Đường cong Bézier để tạo bo góc
+                        lineTo(width, 0f)  // Đường thẳng đứng thứ hai
+                    }
+
+                    drawPath(
+                        path = path,
+                        color = Color.Black,
+                        style = Stroke(
+                            width = 10f,
+                            cap = StrokeCap.Round,
+                            join = StrokeJoin.Round
+                        )
                     )
-                )
+                }
             }
-            Canvas(
+
+            Box(
                 modifier = Modifier
-                    .size(80.dp)
-                    .padding(top = 10.dp, end = 10.dp)
-
+                    .size(100.dp)
+                    .background(Color.Transparent)
+                    .constrainAs(box3) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(box2.start)
+                        top.linkTo(box2.bottom)
+                    }
             ) {
-                val width = size.width
-                val height = size.height
-                val path = Path().apply {
-                    moveTo(width, height)  // Điểm bắt đầu của đường đầu tiên
-                    lineTo(width, height / 2)  // Đường ngang đầu tiên
-                    quadraticTo(
-                        width,
-                        0f,
-                        width / 2,
-                        0f
-                    ) // Đường cong Bézier để tạo bo góc
-                    lineTo(0f, 0f)  // Đường thẳng đứng thứ hai
-                }
+                Canvas(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(bottom = 10.dp, start = 10.dp)
 
-                drawPath(
-                    path = path,
-                    color = Color.Black,
-                    style = Stroke(
-                        width = 10f,
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Round
+                ) {
+                    val width = size.width
+                    val height = size.height
+                    val path = Path().apply {
+                        moveTo(0f, 0f)  // Điểm bắt đầu của đường đầu tiên
+                        lineTo(0f, height / 2)  // Đường ngang đầu tiên
+                        quadraticTo(
+                            0f,
+                            height,
+                            width / 2,
+                            height
+                        ) // Đường cong Bézier để tạo bo góc
+                        lineTo(width, height)  // Đường thẳng đứng thứ hai
+                    }
+
+                    drawPath(
+                        path = path,
+                        color = Color.Black,
+                        style = Stroke(
+                            width = 10f,
+                            cap = StrokeCap.Round,
+                            join = StrokeJoin.Round
+                        )
                     )
-                )
-            }
-        }
-
-
-
-        Box(modifier = Modifier.size(100.dp).background(Color.Transparent).align(
-            Alignment.BottomEnd
-        )){
-            Canvas(
-                modifier = Modifier
-                    .size(80.dp)
-                    .padding(bottom = 10.dp, end = 10.dp)
-
-            ) {
-                val width = size.width
-                val height = size.height
-                val path = Path().apply {
-                    moveTo(0f, height)  // Điểm bắt đầu của đường đầu tiên
-                    lineTo(width / 2, height)  // Đường ngang đầu tiên
-                    quadraticTo(
-                        width,
-                        height,
-                        width,
-                        height / 2
-                    ) // Đường cong Bézier để tạo bo góc
-                    lineTo(width, 0f)  // Đường thẳng đứng thứ hai
                 }
-
-                drawPath(
-                    path = path,
-                    color = Color.Black,
-                    style = Stroke(
-                        width = 10f,
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Round
-                    )
-                )
-            }
-        }
-
-        Box(modifier = Modifier.size(100.dp).background(Color.Transparent).align(
-            Alignment.BottomStart
-        )){
-            Canvas(
-                modifier = Modifier
-                    .size(80.dp)
-                    .padding(bottom = 10.dp, start = 10.dp)
-
-            ) {
-                val width = size.width
-                val height = size.height
-                val path = Path().apply {
-                    moveTo(0f, 0f)  // Điểm bắt đầu của đường đầu tiên
-                    lineTo(0f, height / 2)  // Đường ngang đầu tiên
-                    quadraticTo(0f, height, width / 2, height) // Đường cong Bézier để tạo bo góc
-                    lineTo(width, height)  // Đường thẳng đứng thứ hai
-                }
-
-                drawPath(
-                    path = path,
-                    color = Color.Black,
-                    style = Stroke(
-                        width = 10f,
-                        cap = StrokeCap.Round,
-                        join = StrokeJoin.Round
-                    )
-                )
             }
         }
     }
